3f7411696fcc18a018debc413bcc7148ed435be3
nicholas <nnnnicholas@users.noreply.github.com>
2021-11-26 00:11:11 -0500

    updated contract with OZ imports + new functions

--- contracts/Leaderboard.sol
+++ contracts/Leaderboard.sol
@@ -1,25 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /**
  * @title Leaderboard
  * @dev Receive ETH and associate contributions with contract addresses.
  */
-contract Leaderboard is Ownable{
-
+contract Leaderboard is Ownable, ReentrancyGuard{
     mapping(address => uint256) attention;
 
     event attentionDrawnTo(address focus, uint256 amount);
+    event attentionReset(address _contract);
+
     /**
      * @dev Store cumulative value in attention mapping
      * @param focus to pay attention to
      */
-    function payAttention(address focus) external payable{
+    function payAttention(address focus) external payable nonReentrant{
         attention[focus] += msg.value;
         emit attentionDrawnTo(focus, msg.value);
     }
@@ -28,28 +28,34 @@ contract Leaderboard is Ownable{
      * @dev Retrieve attention paid to a given address
      * @return attention measured in wei
      */
-    function retrieve(address focus) external view returns (uint256){
+    function retrieve(address focus) external view returns (uint256) {
         return attention[focus];
     }
 
-    function withdraw() external payable onlyOwner{
+    function withdraw() external payable onlyOwner {
         uint256 balance = address(this).balance;
-        require(balance > 0, 'Contract contains no ETH');
+        require(balance > 0, "Contract contains no ETH");
         address payable owner = payable(owner());
         (bool success, ) = owner.call{value: msg.value}("");
         require(success, "Failed to send Ether");
     }
 
-    function withdrawTo(address payable _to, uint _amount) public {
+    function withdrawTo(address payable _to, uint256 _amount) public nonReentrant {
         (bool success, ) = _to.call{value: _amount}("");
         require(success, "Failed to send Ether");
     }
 
-    function getBalance() public view returns (uint) {
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    receive() external payable{}
-    fallback() external payable{}
+    function resetAttention(address _contract) external onlyOwner nonReentrant{
+        attention[_contract] = 0;
+        emit attentionReset(_contract);
 
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
